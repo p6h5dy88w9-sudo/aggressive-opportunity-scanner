a821a9faba1c7f93faf6af8de397bd3b82de7eb7
p6h5dy88w9-sudo <p6h5dy88w9@privaterelay.appleid.com>
2026-08-08 15:38:21 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='aggressive-scanner-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)));});
